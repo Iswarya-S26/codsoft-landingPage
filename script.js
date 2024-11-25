fcaa92091426a920da8ecc52af35bd58bbcce261
Iswarya-S26 <emails2iswarya2602@gmail.com>
2024-11-25 21:09:07 +0000

    set menu button

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const nav = document.getElementsByTagName("nav")[0];
 
 window.addEventListener("scroll", () => {
   nav.classList.toggle("sticky", window.scrollY > 60);
+});
+
+let menu = document.getElementById("menu-icon");
+let navbar = document.querySelector(".navbar-ul");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
   console.log("hi");
 });
